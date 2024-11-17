2b505a701a9aadfcc8a0f565cbe912e8496272ed
Danielle Voznyy <dan.voznyy@gmail.com>
2024-11-16 19:06:34 -0500

    Remove motion canvas, will be generating simple slides instead

--- src/project.ts
+++
@@ -1,8 +0,0 @@
-import {makeProject} from '@motion-canvas/core';
-import audio from '../audio/test.mp3';
-import example from './scenes/example?scene';
-import example2 from "./scenes/example2?scene";
-
-export default makeProject({
-  scenes: [example, example2],
-});
